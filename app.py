b6845abf57763539838772750f7277738ee83d2b
Rohit Mukati <rohanmukati2002@gmail.com>
2023-03-04 18:12:37 +0530

    first project

--- app.py
+++ app.py
@@ -32,16 +32,14 @@ JOBS = [
     }
 ]
 
-@app.route("/api/jobs")
+@app.route("/")
 def hello_Rohit():
     return render_template('home.html',
                            jobs=JOBS, company_name='IT Park')
-    
-    
-@app.route("/")
+
+@app.route("/api/jobs")
 def list_jobs():
     return jsonify(JOBS)
- 
-    
+
 if __name__ == '__main__':
     app.run(host='0.0.0.0',debug=True)
